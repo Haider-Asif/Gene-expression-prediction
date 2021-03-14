d20c291ca393f5c34fdf11de72264eea512c6f89
Haider-Asif <60201762+Haider-Asif@users.noreply.github.com>
2021-03-14 03:40:00 -0400

    updates to generate CSV

--- model.py
+++ model.py
@@ -3,16 +3,9 @@ import tensorflow as tf
 from matplotlib import pyplot as plt
 from scipy.stats import pearsonr
 from sklearn.metrics import mean_squared_error
-import math
+import pandas as pd
 
-def get_data():
-    # Keys to npzfile of train & eval
-    train_cells = ['E065', 'E004', 'E066', 'E005', 'E012', 'E027', 'E053', 'E013', 'E028', 'E061', 'E109', 'E120', 'E062', 'E037', 'E038', 'E024', 'E105', 'E011', 'E106', 'E082', 'E097', 'E116', 'E098', 'E058', 
-    'E117', 'E059', 'E070', 'E118', 'E085', 'E104', 'E119', 'E006', 'E127', 'E047', 'E094', 'E007', 'E054', 'E128', 'E095', 'E055', 'E114', 'E100', 'E056', 'E016', 'E122', 'E057', 'E123', 'E079', 'E003', 'E050']
-
-    eval_cells = ['E065', 'E004', 'E066', 'E005', 'E012', 'E027', 'E053', 'E013', 'E028', 'E061', 'E109', 'E120', 'E062', 'E037', 'E038', 'E024', 'E071', 'E105', 'E087', 'E011', 'E106', 'E096', 'E082', 'E097', 
-    'E116', 'E098', 'E058', 'E117', 'E084', 'E059', 'E070', 'E118', 'E085', 'E104', 'E119', 'E006', 'E112', 'E127', 'E047', 'E094', 'E007', 'E054', 'E113', 'E128', 'E095', 'E055', 'E114', 'E100', 'E056', 'E016', 'E122', 'E057', 'E123', 'E079', 'E003', 'E050']
-
+def get_data(train_cells,eval_cells):
     # Load data
     train_data = np.load('../train.npz')
     eval_data = np.load('../eval.npz')
@@ -39,7 +32,7 @@ def get_data():
 
     eval_inputs = np.concatenate(eval_inputs, axis=0)
 
-    return train_inputs, train_outputs, eval_inputs
+    return train_inputs, train_outputs, eval_inputs,eval_data
 
 def k_cross_validate_model(train_x, train_y, k):
     for i in range(k):
@@ -75,20 +68,20 @@ def train_model(train_x, train_y):
     batch_norm_1 = tf.keras.layers.BatchNormalization()
     max_pool_1 = tf.keras.layers.MaxPool1D(3)
     flatten = tf.keras.layers.Flatten()
-    Dense_1 = tf.keras.layers.Dense(32,activation=tf.keras.layers.ReLU())
-    dropout_1 = tf.keras.layers.Dropout(0.3)
+    dropout = tf.keras.layers.Dropout(0.3)
+    Dense_1 = tf.keras.layers.Dense(64,activation=tf.keras.layers.ReLU())
     Dense_2 = tf.keras.layers.Dense(32,activation=None)
-    dropout_2 = tf.keras.layers.Dropout(0.3)
     Dense_3 = tf.keras.layers.Dense(1,activation=tf.keras.layers.ReLU())
     model.add(layer_1)
     model.add(batch_norm_1)
     model.add(max_pool_1)
     model.add(flatten)
-    model.add(Dense_1)
-    model.add(dropout_1)
-    model.add(Dense_2)
-    model.add(dropout_2)
+    model.add(dropout)
     model.add(Dense_3)
+    # model.add(dropout)
+    # model.add(Dense_2)
+    # model.add(dropout)
+    # model.add(Dense_3)
     # k_cross_validate_model(train_x,train_y,5)
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.01), loss=tf.keras.losses.MeanSquaredError())
     model.fit(x=train_x, y=train_y, batch_size=100, epochs=10,shuffle=True)
@@ -119,9 +112,33 @@ def generate_plots(train_x,prediction, num_images):
         ax2.title.set_text("predicted high-resolution output")
         plt.show()
 
+def generate_csv(eval_inputs,eval_cells,eval_data):
+    cell_list = []
+    gene_list = []
+    example_eval_preds = np.random.randn(len(eval_inputs))
+    for cell in eval_cells:
+        cell_data = eval_data[cell]
+        cell_list.extend([cell]*len(cell_data))
+        genes = cell_data[:,0,0].astype('int32')
+        gene_list.extend(genes)
+
+    id_column = [] # ID is {cell type}_{gene id}
+    for idx in range(len(eval_inputs)):
+        id_column.append(f'{cell_list[idx]}_{gene_list[idx]}')
+
+    df_data = {'id': id_column, 'expression' : example_eval_preds}
+    submit_df = pd.DataFrame(data=df_data)
+
+    submit_df.to_csv('../sample_submission.csv', header=True, index=False, index_label=False)
+
 def main():
+        # Keys to npzfile of train & eval
+    train_cells = ['E065', 'E004', 'E066', 'E005', 'E012', 'E027', 'E053', 'E013', 'E028', 'E061', 'E109', 'E120', 'E062', 'E037', 'E038', 'E024', 'E105', 'E011', 'E106', 'E082', 'E097', 'E116', 'E098', 'E058', 
+    'E117', 'E059', 'E070', 'E118', 'E085', 'E104', 'E119', 'E006', 'E127', 'E047', 'E094', 'E007', 'E054', 'E128', 'E095', 'E055', 'E114', 'E100', 'E056', 'E016', 'E122', 'E057', 'E123', 'E079', 'E003', 'E050']
+    eval_cells = ['E065', 'E004', 'E066', 'E005', 'E012', 'E027', 'E053', 'E013', 'E028', 'E061', 'E109', 'E120', 'E062', 'E037', 'E038', 'E024', 'E071', 'E105', 'E087', 'E011', 'E106', 'E096', 'E082', 'E097', 
+    'E116', 'E098', 'E058', 'E117', 'E084', 'E059', 'E070', 'E118', 'E085', 'E104', 'E119', 'E006', 'E112', 'E127', 'E047', 'E094', 'E007', 'E054', 'E113', 'E128', 'E095', 'E055', 'E114', 'E100', 'E056', 'E016', 'E122', 'E057', 'E123', 'E079', 'E003', 'E050']
     # Call get_data() to read in all of the data
-    train_x, train_y, test_x = get_data()
+    train_x, train_y, test_x, test_data = get_data(train_cells,eval_cells)
     # Call remove_borders() to properly modify the training labels
     # Call train_model() to train the model
     model = train_model(train_x,train_y)
@@ -132,6 +149,7 @@ def main():
     pearsons,loss = evaluation_metrics(train_prediction, train_y)
     print("Pearsons correlation co-efficent: ", pearsons)
     print("Average final Mean squared error loss: ",loss)
+    generate_csv(test_x,eval_cells, test_data)
     # generate_plots(train_y,train_prediction,100)
     # generate_plots(test_x,test_prediction,100)
 

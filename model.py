f55f87861f4619a21e48e08aceb58ac62515f386
Isaac Nathoo <37516124+isaacnathoo@users.noreply.github.com>
2021-03-15 13:50:14 -0400

    update 302

--- model.py
+++ model.py
@@ -40,66 +40,15 @@ def k_cross_validate_model(train_x, train_y, k):
         validation_y = train_y[int(i*(1/k)*train_y.shape[0]):int((i+1)*(1/k)*train_y.shape[0])]
         training_x = np.concatenate((train_x[0:int(i*(1/k)*train_x.shape[0])],train_x[int((i+1)*(1/k)*train_x.shape[0]):train_x.shape[0]]), axis=0)
         training_y = np.concatenate((train_y[0:int(i*(1/k)*train_y.shape[0])],train_y[int((i+1)*(1/k)*train_y.shape[0]):train_y.shape[0]]), axis=0)
-        model = tf.keras.Sequential()
-        layer_1 = tf.keras.layers.Conv1D(50,10,activation=tf.keras.layers.LeakyReLU(0.05), padding="SAME")
-        batch_norm_1 = tf.keras.layers.BatchNormalization()
-        max_pool_1 = tf.keras.layers.MaxPool1D(5)
 
-        layer_2 = tf.keras.layers.Conv1D(50,5,activation=tf.keras.layers.LeakyReLU(0.05), padding="SAME")
-        batch_norm_2 = tf.keras.layers.BatchNormalization()
-        max_pool_2 = tf.keras.layers.MaxPool1D(2)
-
-        layer_3 = tf.keras.layers.Conv1D(50,5,activation=tf.keras.layers.LeakyReLU(0.05), padding="SAME")
-        batch_norm_3 = tf.keras.layers.BatchNormalization()
-        max_pool_3 = tf.keras.layers.MaxPool1D(5)
-        flatten = tf.keras.layers.Flatten()
-
-        lstm = tf.keras.layers.LSTM(200, return_sequences=True)
-
-        dropout1 = tf.keras.layers.Dropout(0.5)
-        Dense_1 = tf.keras.layers.Dense(625,activation=tf.keras.layers.LeakyReLU(0.05))
-        Dense_2 = tf.keras.layers.Dense(125,activation=tf.keras.layers.LeakyReLU(0.05))
-        Dense_3 = tf.keras.layers.Dense(25,activation=tf.keras.layers.LeakyReLU(0.05))
-        Dense_4 = tf.keras.layers.Dense(1,activation=None)
-        model.add(layer_1)
-        model.add(batch_norm_1)
-        model.add(max_pool_1)
-
-        model.add(layer_2)
-        model.add(batch_norm_2)
-        model.add(max_pool_2)
-
-        model.add(layer_3)
-        model.add(batch_norm_3)
-        model.add(max_pool_3)
-
-        model.add(flatten)
-        # model.add(lstm)
-        model.add(dropout1)
-        model.add(Dense_1)
-        # model.add(dropout1)
-        model.add(Dense_2)
-        # model.add(dropout1)
-        # model.add(Dense_3)
-        model.add(Dense_4)
-        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.01), loss=tf.keras.losses.MeanSquaredError())
-        model.fit(x=training_x, y=training_y, batch_size=128, epochs=20, validation_data=(validation_x,validation_y), shuffle=True)
-
-def train_model(train_x, train_y):
-    """
-    Implements and trains the model using a cross-validation scheme with MSE loss
-    param train_x: the training inputs
-    param train_y: the training labels
-    return: a trained model
-    """
-    model = tf.keras.Sequential()
+            model = tf.keras.Sequential()
     layer_1 = tf.keras.layers.Conv1D(50,10,activation=tf.keras.layers.LeakyReLU(0.05), padding="SAME")
     batch_norm_1 = tf.keras.layers.BatchNormalization()
     max_pool_1 = tf.keras.layers.MaxPool1D(5)
 
     layer_2 = tf.keras.layers.Conv1D(50,5,activation=tf.keras.layers.LeakyReLU(0.05), padding="SAME", dilation_rate=2)
     batch_norm_2 = tf.keras.layers.BatchNormalization()
-    max_pool_2 = tf.keras.layers.MaxPool1D(2)
+    max_pool_2 = tf.keras.layers.MaxPool1D(3)
 
     layer_3 = tf.keras.layers.Conv1D(50,3,activation=tf.keras.layers.LeakyReLU(0.05), padding="SAME")
     batch_norm_3 = tf.keras.layers.BatchNormalization()
@@ -136,7 +85,60 @@ def train_model(train_x, train_y):
     model.add(Dense_4)
     # k_cross_validate_model(train_x,train_y,4)
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.01), loss=tf.keras.losses.MeanSquaredError())
-    model.fit(x=train_x, y=train_y, batch_size=250, epochs=20,shuffle=True)
+    model.fit(x=training_x, y=training_y, batch_size=250, epochs=30, validation_data=(validation_x,validation_y), shuffle=True)
+
+def train_model(train_x, train_y):
+    """
+    Implements and trains the model using a cross-validation scheme with MSE loss
+    param train_x: the training inputs
+    param train_y: the training labels
+    return: a trained model
+    """
+    model = tf.keras.Sequential()
+    layer_1 = tf.keras.layers.Conv1D(50,10,activation=tf.keras.layers.LeakyReLU(0.05), padding="SAME")
+    batch_norm_1 = tf.keras.layers.BatchNormalization()
+    max_pool_1 = tf.keras.layers.MaxPool1D(5)
+
+    layer_2 = tf.keras.layers.Conv1D(50,5,activation=tf.keras.layers.LeakyReLU(0.05), padding="SAME", dilation_rate=2)
+    batch_norm_2 = tf.keras.layers.BatchNormalization()
+    max_pool_2 = tf.keras.layers.MaxPool1D(3)
+
+    layer_3 = tf.keras.layers.Conv1D(50,3,activation=tf.keras.layers.LeakyReLU(0.05), padding="SAME")
+    batch_norm_3 = tf.keras.layers.BatchNormalization()
+    max_pool_3 = tf.keras.layers.MaxPool1D(5)
+    flatten = tf.keras.layers.Flatten()
+
+    lstm = tf.keras.layers.LSTM(200, return_sequences=True)
+
+    dropout1 = tf.keras.layers.Dropout(0.5)
+    Dense_1 = tf.keras.layers.Dense(625,activation=tf.keras.layers.LeakyReLU(0.05))
+    Dense_2 = tf.keras.layers.Dense(125,activation=tf.keras.layers.LeakyReLU(0.05))
+    Dense_3 = tf.keras.layers.Dense(25,activation=tf.keras.layers.LeakyReLU(0.05))
+    Dense_4 = tf.keras.layers.Dense(1,activation=None)
+    model.add(layer_1)
+    model.add(batch_norm_1)
+    model.add(max_pool_1)
+
+    model.add(layer_2)
+    model.add(batch_norm_2)
+    model.add(max_pool_2)
+
+    model.add(layer_3)
+    model.add(batch_norm_3)
+    model.add(max_pool_3)
+
+    model.add(flatten)
+    # model.add(lstm)
+    model.add(dropout1)
+    model.add(Dense_1)
+    # model.add(dropout1)
+    model.add(Dense_2)
+    # model.add(dropout1)
+    # model.add(Dense_3)
+    model.add(Dense_4)
+    # k_cross_validate_model(train_x,train_y,4)
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.01), loss=tf.keras.losses.MeanSquaredError())
+    model.fit(x=train_x, y=train_y, batch_size=250, epochs=30,shuffle=True)
     return model
 
 def make_prediction(model, input_data):

93128dae312c66a5b4f2f54603ec424d0bd4079e
Haider-Asif <60201762+Haider-Asif@users.noreply.github.com>
2021-03-16 01:27:09 -0400

    push

--- model.py
+++ model.py
@@ -98,11 +98,11 @@ def train_model(train_x, train_y):
     batch_norm_1 = tf.keras.layers.BatchNormalization()
     max_pool_1 = tf.keras.layers.MaxPool1D(5)
 
-    layer_2 = tf.keras.layers.Conv1D(50,5,activation=tf.keras.layers.LeakyReLU(0.05), padding="SAME", dilation_rate=3)
+    layer_2 = tf.keras.layers.Conv1D(25,5,activation=tf.keras.layers.LeakyReLU(0.05), padding="SAME", dilation_rate=3)
     batch_norm_2 = tf.keras.layers.BatchNormalization()
     max_pool_2 = tf.keras.layers.MaxPool1D(3)
 
-    layer_3 = tf.keras.layers.Conv1D(50,3,activation=tf.keras.layers.LeakyReLU(0.05), padding="SAME", dilation_rate=2)
+    layer_3 = tf.keras.layers.Conv1D(10,3,activation=tf.keras.layers.LeakyReLU(0.05), padding="SAME", dilation_rate=2)
     batch_norm_3 = tf.keras.layers.BatchNormalization()
     max_pool_3 = tf.keras.layers.MaxPool1D(5)
     flatten = tf.keras.layers.Flatten()

f2b30b762aff2f733a605cb35527b6f00034d809
Haider-Asif <60201762+Haider-Asif@users.noreply.github.com>
2021-03-16 23:50:32 -0400

    a

--- model.py
+++ model.py
@@ -96,9 +96,9 @@ def train_model(train_x, train_y):
     return: a trained model
     """
     model = tf.keras.Sequential()
-    layer_1 = tf.keras.layers.Conv1D(50,5,activation=tf.keras.layers.LeakyReLU(0.05), padding="SAME")
+    layer_1 = tf.keras.layers.Conv1D(50,10,activation=tf.keras.layers.LeakyReLU(0.05), padding="SAME")
     batch_norm_1 = tf.keras.layers.BatchNormalization()
-    max_pool_1 = tf.keras.layers.MaxPool1D(2)
+    max_pool_1 = tf.keras.layers.MaxPool1D(5)
 
     layer_2 = tf.keras.layers.Conv1D(50,5,activation=tf.keras.layers.LeakyReLU(0.05), padding="SAME", dilation_rate=3)
     batch_norm_2 = tf.keras.layers.BatchNormalization()
